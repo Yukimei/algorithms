fc88e0f1dda0ee1cfdd419c3bbdcfdb4b9246e58
yingxuemei <yingxuemei@yingxues-mbp.wv.cc.cmu.edu>
2016-04-05 18:57:18 -0400

    a

--- LeetCode/src/Tree/LargestBSTSubTree.java
+++ LeetCode/src/Tree/LargestBSTSubTree.java
@@ -1,48 +1,42 @@
 package Tree;
-
+// key : 
+// 1. goes to the very deep (so traverse first)
+// 2. write return type so by call recursion can know the certain node's size, upper bound, lower bound
+// (just as dp)
 public class LargestBSTSubTree {
-	private int sum = 0;
+	private int max = 0;
 
 	public int largestBSTSubtree(TreeNode root) {
 		if (root == null) {
 			return 0;
 		}
-		dfs(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
-		return sum == 0 ? 1 : sum;
+		traverse(root);
+		return max;
 	}
 
-	private int dfs(TreeNode root, int min, int max) { 
-	        if (root == null) {
-	            return 0;
-	        }
-	        
-	        int left = 0;
-	        if (left! = null && left.val > root.val) {
-	            left = dfs(root.left, Integer.MIN_VALUE, Integer.MAX_VALUE);
-	            return -1;
-	        } else {
-	            left = dfs(root.left, min, root.val);
-	        }
-	        
-	        int right = 0;
-	        if (right! = null && right.val < root.val) {
-	            right = dfs(root.right, Integer.MIN_VALUE, Integer.MAX_VALUE);
-	            return -1;
-	        } else {
-	            right = dfs(root.right, root.val, max);
-	        }
-	        
-	        if (left != -1 && right != -1) {
-	            System.out.println(" root.val" + root.val);
-	            System.out.println(" change max");
-	            System.out.println(" max =" + sum);
-	            System.out.println(" left = " + left);
-	             System.out.println(" right = " + right);
-	            sum = Math.max(left + right + 1, sum);
-	             System.out.println(" max =" + sum);
-	        }
-	        
-	        
-	        return left == -1 || right == -1 ? -1 : root.val < min || root.val > max ? -1 : left + right + 1;
-	    }
+	private Result traverse(TreeNode root) {
+		if (root == null) {
+			return new Result(0, Integer.MAX_VALUE, Integer.MIN_VALUE);
+		}
+		Result left = traverse(root.left);
+		Result right = traverse(root.right);
+		if (left.size == -1 || right.size == -1 || left.upper >= root.val || right.lower <= root.val) {
+			return new Result(-1, 0, 0);
+		}
+		int size = left.size + right.size + 1;
+		max = Math.max(size, max);
+		return new Result(size, Math.min(root.val, left.lower), Math.max(root.val, right.upper));
+	}
+
+	class Result {
+		int size;
+		int lower;
+		int upper;
+
+		public Result(int s, int l, int u) {
+			size = s;
+			lower = l;
+			upper = u;
+		}
+	}
 }
